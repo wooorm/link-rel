a05ebacc63061629234915b56b711a0fe038a247
Titus Wormer <tituswormer@gmail.com>
2017-06-25 19:15:56 +0200

    Refactor build script

--- build.js
+++ build.js
@@ -6,10 +6,9 @@ var concat = require('concat-stream');
 var bail = require('bail');
 
 https.get('https://help.whatwg.org/extensions/link-rel/', function (res) {
-  res
-    .pipe(concat(function (buf) {
-      var value = String(buf).split('\n').sort().filter(Boolean);
-      fs.writeFile('index.json', JSON.stringify(value, 0, 2) + '\n', bail);
-    }))
-    .on('error', bail);
+  res.pipe(concat(onconcat)).on('error', bail);
+  function onconcat(buf) {
+    var value = String(buf).split('\n').sort().filter(Boolean);
+    fs.writeFile('index.json', JSON.stringify(value, 0, 2) + '\n', bail);
+  }
 });
