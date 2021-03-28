a5ec13ad4ef137e70d5b4da394cc35381e367963
Titus Wormer <tituswormer@gmail.com>
2021-03-28 16:43:32 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -8,9 +8,10 @@ test('linkRel', function (t) {
   t.ok(Array.isArray(linkRel), 'should be an `array`')
 
   t.doesNotThrow(function () {
-    linkRel.forEach(function (tagName) {
-      assert.strictEqual(typeof tagName, 'string', tagName)
-    })
+    var index = -1
+    while (++index < linkRel.length) {
+      assert.strictEqual(typeof linkRel[index], 'string', linkRel[index])
+    }
   }, 'should be a `string`s')
 
   t.end()
