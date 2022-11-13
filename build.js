a073dbb3384b036de3a99d14b1fce70a1615dfa5
Titus Wormer <tituswormer@gmail.com>
2022-11-13 14:38:59 +0100

    Refactor build script

--- build.js
+++ build.js
@@ -1,62 +1,40 @@
-/**
- * @typedef {import('hast').Element} Element
- */
-
-import fs from 'node:fs'
-import https from 'node:https'
-import concat from 'concat-stream'
-import {bail} from 'bail'
-import {unified} from 'unified'
-import rehypeParse from 'rehype-parse'
+import fs from 'node:fs/promises'
+import fetch from 'node-fetch'
+import {fromHtml} from 'hast-util-from-html'
 import {select, selectAll} from 'hast-util-select'
 import {toString} from 'hast-util-to-string'
 
-const proc = unified().use(rehypeParse)
+const response = await fetch(
+  'https://microformats.org/wiki/existing-rel-values'
+)
+const text = await response.text()
 
-https.get('https://microformats.org/wiki/existing-rel-values', onconnection)
+const tree = fromHtml(text)
+const value = table('formats').concat(table('HTML5_link_type_extensions'))
 
-/**
- * @param {import('http').IncomingMessage} response
- */
-function onconnection(response) {
-  response.pipe(concat(onconcat)).on('error', bail)
+if (value.length === 0) {
+  throw new Error('Couldn’t find any rels')
 }
 
+await fs.writeFile(
+  'index.js',
+  'export const linkRel = ' + JSON.stringify(value.sort(), null, 2) + '\n'
+)
+
 /**
- * @param {Buffer} buf
+ * @param {string} name
  */
-function onconcat(buf) {
-  const tree = proc.parse(buf)
-  const value = table('formats').concat(table('HTML5_link_type_extensions'))
+function table(name) {
+  const node = select('h2:has(#' + name + ') ~ table', tree)
 
-  if (value.length === 0) {
-    bail(new Error('Couldn’t find any rels'))
+  if (!node) {
+    throw new Error('Missing node for name `' + name + '`')
   }
 
-  fs.writeFile(
-    'index.js',
-    'export const linkRel = ' + JSON.stringify(value.sort(), null, 2) + '\n',
-    bail
-  )
+  const rows = selectAll('tr', node).slice(1)
 
-  /**
-   * @param {string} name
-   */
-  function table(name) {
-    const node = select('h2:has(#' + name + ') ~ table', tree)
-
-    if (!node) {
-      throw new Error('Missing node for name `' + name + '`')
-    }
-
-    /** @type {Element[]} */
-    const rows = selectAll('tr', node).slice(1)
-
-    return rows
-      .map((row) =>
-        selectAll('td', row).map((/** @type {Element} */ d) => toString(d))
-      )
-      .filter((cells) => !/not allowed/i.test(cells[1].trim()))
-      .map((cells) => cells[0].trim())
-  }
+  return rows
+    .map((row) => selectAll('td', row).map((d) => toString(d).trim()))
+    .filter((cells) => !/not allowed/i.test(cells[1]))
+    .map((cells) => cells[0])
 }
